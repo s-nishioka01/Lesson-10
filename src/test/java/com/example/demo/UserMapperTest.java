70ca86972dbc36ab4bb20ac6fe17a9c8864b7c9f
s.n <s.n.@sns-MacBook-Air-2020.local>
2022-07-14 17:34:50 +0900

    不要なimportを削除

--- src/test/java/com/example/demo/UserMapperTest.java
+++ src/test/java/com/example/demo/UserMapperTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.demo.entity.User;
 import com.example.demo.form.UserForm;
